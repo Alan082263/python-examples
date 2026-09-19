8df988dfe9983e693ac03dea96b630e31b875d4f
Alan082263 <parcontroller@gmail.com>
2026-09-19 14:21:05 -0700

    commit

---
+++ Guess the Number/guess_the_number.py
@@ -0,0 +1,9 @@
+import random
+
+print("I have selected a number between 1 and 9. Can you guess it?")
+secret_num = random.randint(1, 9)
+guess = int(input("Enter your guess: "))
+if guess == secret_num:
+    print("Congratulations! You guessed correctly.")
+else:
+    print("Wrong guess. The number was:", secret_num)
